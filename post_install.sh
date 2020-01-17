553408787af6423744874d0c1ae385c852e392ab
citaty-svyatyh <46151646+citaty-svyatyh@users.noreply.github.com>
2020-01-17 10:34:44 +0300

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -81,7 +81,7 @@ if [ `whoami` = root ]; then
         check_yes_no 'yum -q -y install npm'
         # Нужен, чтобы заработала автодополнялка js
         check_yes_no 'npm install -g tern'
-        check_yes_no 'npm install -g eslint'
+        # check_yes_no 'npm install -g eslint' ставится локально
 
 
         #-------------------------------------------------------------------------------
@@ -161,8 +161,8 @@ check_yes_no 'echo "export MYCOLOR=24bit" >> .bash_profile'
 echo "Настройка глобального eslintera. Для react это не требуется! В react достаточно одной строки в package.json, которая есть по умолчанию"
 read -p "Нужно настроить глобальный eslinter (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
-    #check_yes_no 'npm init'
-    #check_yes_no 'npm install eslint --save-dev'
+    check_yes_no 'npm init'
+    check_yes_no 'npm install eslint --save-dev'
     echo "choise 'To check syntax, find problems, and enforce code style'"
     check_yes_no 'eslint --init'
 fi
